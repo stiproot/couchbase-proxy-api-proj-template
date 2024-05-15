b1222cc13818001f96698e8f0aac1aa0b083cc65
Simon Stipcich <simon.stipcich@derivco.co.za>
2024-05-15 16:47:27 +0200

    feat:k8s manifests.

--- .k8s/manifests/apply.sh
+++ .k8s/manifests/apply.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-kubectl apply -f project-m-ui.yml
-kubectl apply -f project-m-ui-api.yml
-kubectl apply -f project-m-store-api.yml
-kubectl apply -f project-m-kafka-api.yml
-kubectl apply -f project-m-azdo-proxy-api.yml
-kubectl apply -f project-m-azdo-worker.yml
-kubectl apply -f project-m-azdo-proxy-worker.yml
-kubectl apply -f project-m-persist-worker.yml
-kubectl apply -f project-m-insights-worker.yml
-
-# kubectl apply -f cb-proxy-api-db.yml
-# kubectl apply -f kafka/kafka.yml
-# kubectl apply -f kafka/kafka-service.yml
+kubectl apply -f cb-proxy-api-db.yml
+kubectl apply -f cb-proxy-api.yml
--- .k8s/manifests/delete.sh
+++ .k8s/manifests/delete.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-kubectl delete -f kafka/kafka-service.yml
-kubectl delete -f kafka/kafka.yml
-
 kubectl delete -f cb-proxy-api.yml
-kubectl delete -f project-m-kafka-api.yml
-kubectl delete -f project-m-ui-api.yml
-
-kubectl delete -f project-m-azdo-worker.yml
-kubectl delete -f project-m-persist-worker.yml
-kubectl delete -f project-m-insights-worker.yml
-
 kubectl delete -f cb-proxy-api-db.yml
-kubectl delete -f project-m-ui.yml
